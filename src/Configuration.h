76f1855abac39e9e714b8482d349a707701a1325
Álvaro García <maxpowel@gmail.com>
2016-08-24 00:11:35 +0200

    Added main code

---
+++ src/Configuration.h
@@ -0,0 +1,14 @@
+//Configuration
+
+
+// Battery monitor
+#define BATTERY_MONITOR_PIN A0
+#define BATTERY_MONITOR_R1 100000
+#define BATTERY_MONITOR_R2 10000
+#define BATTERY_MONITOR_VOLTAGE_REFERENCE 4.7
+
+// Temperature probe ds18b20
+#define SENSOR_ONE_WIRE_BUS 2
+
+// Temperature and humidity sensors
+#define DHTTYPE DHT11
--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,36 @@
 #include "JsonWriter.h"
 #include <ArduinoJson.h>
+#include <MemoryFree.h>
+#include <avr/pgmspace.h>
+
+/* Sensors dependencies */
+#include "Configuration.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <VoltMeter.h>
+#include <Multiplexer.h>
+#include <SensorMonitor.h>
 
 
+/************************/
+
+/* Simple json message recognizer */
+#define JSON_READ_BUFFER 100
 JsonWriter serialJsonWriter(&Serial);
+
+SensorMonitor m;
+
+/************************/
+/* ds18b20 Sensors      */
+/************************/
+// I have some issues if this is not created here
+OneWire oneWire(SENSOR_ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
 
 struct Command {
     char  command[20];
@@ -11,16 +39,71 @@ struct Command {
 
 
 
-void commandInfo(JsonObject *input, JsonWriter *output){
+void commandStatus(JsonObject *input, JsonWriter *output){
     // Display sensor information
     output->
     beginObject("data")
       .property("status", "ok")
+      .beginObject("memory")
+        .property("available", freeMemory())
+      .endObject()
     .endObject();
 }
 
-#define TOTAL_COMMANDS 1
-struct Command commandList[TOTAL_COMMANDS] = {
+void commandRead(JsonObject *input, JsonWriter *output){
+    float multipleValues[10];
+    Device *device;
+    output->
+    beginArray("data");
+
+    for (int i = 0; i < m.getTotalDevices(); i++) {
+        device = m.getDevice(i);
+        output->beginObject()
+          .property("name", device->getName());
+        if (device->multipleValues()) {
+          int totalValues = device->getValues(multipleValues);
+          output->
+            property("multiple", true)
+            .property("total", totalValues)
+            .beginArray("values");
+
+            for (int j = 0; j < totalValues; j++) {
+              output->number(multipleValues[j]);
+            }
+          output->endArray();
+        } else {
+          output->
+            property("multiple", false)
+            .property("value", device->getValue());
+        }
+        output->endObject();
+    }
+    output->endArray();
+    //Fix this bug in JsonWriter, when array is empty no separator is printed
+    if(m.getTotalDevices() == 0)
+      output->separator();
+}
+
+void commandInfo(JsonObject *input, JsonWriter *output){
+    // Display sensor information
+    Device *device;
+    output->beginArray("data");
+
+    for (int i = 0; i < m.getTotalDevices(); i++) {
+        device = m.getDevice(i);
+        output->beginObject()
+          .property(device->getName(), (char *)device->getSensorType())
+        .endObject();
+
+    }
+    output->endArray();
+}
+
+
+#define TOTAL_COMMANDS 3
+const struct Command commandList[TOTAL_COMMANDS] = {
+        {"read", commandRead},
+        {"status", commandStatus},
         {"info", commandInfo}
 };
 
@@ -28,20 +111,21 @@ struct Command commandList[TOTAL_COMMANDS] = {
 void setup() {
   // put your setup code here, to run once:
 
-  Serial.begin(9600);
+  Serial.begin(115200);
+  //Voltmeter
+  VoltmeterDevice *voltmeter = new VoltmeterDevice(BATTERY_MONITOR_PIN, BATTERY_MONITOR_R1, BATTERY_MONITOR_R2, BATTERY_MONITOR_VOLTAGE_REFERENCE);
+  m.addDevice(voltmeter);
 
 
 }
 
 
-/* Simple json message recognizer */
-#define JSON_READ_BUFFER 180
+
 
 char readBuffer[JSON_READ_BUFFER];
 int pairingLeft = 0;
 char bufferPosition = 0;
 void loop() {
-
 
     while(Serial.available()){
         char c = Serial.read();
@@ -51,7 +135,6 @@ void loop() {
         } else if (c == '}') {
             pairingLeft--;
         }
-
         readBuffer[bufferPosition] = c;
         bufferPosition++;
 
